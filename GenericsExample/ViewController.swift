f4813b032489c0e4e3ba20c2fa56bf2e73308182
Ryan Cortez <ryancortez@me.com>
2016-08-09 16:25:13 -0500

    - Created a simple example for a function that takes a generic type

--- GenericsExample/ViewController.swift
+++ GenericsExample/ViewController.swift
@@ -9,17 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let intNumbers:Array<Int> = [1,3,45,2,3,10,60]
+    let floatNumbers:Array<Float> = [1,3,45,2,3,10,60]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let result = filter(floatNumbers) { (number :Float) -> Bool in
+            if (number < 100.0) {
+                return true
+            } else {
+                return false
+            }
+        }
+        print(result)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func filter<Type>(numbers :[Type], includeNumbers : (Type) -> Bool) -> [Type] {
+        var results = [Type]()
+        for number in numbers {
+            if includeNumbers(number) {
+                results.append(number)
+            }
+        }
+        
+        return results
     }
-
-
 }
 
